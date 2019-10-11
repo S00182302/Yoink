c1037174718327ae82940bf78d0729c6bd1960ef
Peter Snee <Peter Snee@DESKTOP-3OF2C5U>
2019-10-11 14:14:05 +0100

    Updated the CLI Creted Tabs, Tabs are working, Added headers to the different tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  { path: 'create-post', loadChildren: './create-post/create-post.module#CreatePostPageModule' },
+  { path: 'favorites', loadChildren: './favorites/favorites.module#FavoritesPageModule' },
+  { path: 'my-profile', loadChildren: './my-profile/my-profile.module#MyProfilePageModule' }
+
 ];
 @NgModule({
   imports: [
--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -8,45 +8,65 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
+        path: 'home',
         children: [
           {
             path: '',
             loadChildren: () =>
-              import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+              import('../home/home.module').then(m => m.HomePageModule)
           }
         ]
       },
       {
-        path: 'tab2',
+        path: 'search',
         children: [
           {
             path: '',
             loadChildren: () =>
-              import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+              import('../search/search.module').then(m => m.SearchPageModule)
           }
         ]
       },
       {
-        path: 'tab3',
+        path: 'create-post',
         children: [
           {
             path: '',
             loadChildren: () =>
-              import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+              import('../create-post/create-post.module').then(m => m.CreatePostPageModule)
+          }
+        ]
+      },
+      {
+        path: 'favorites',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../favorites/favorites.module').then(m => m.FavoritesPageModule)
+          }
+        ]
+      },
+      {
+        path: 'my-profile',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../my-profile/my-profile.module').then(m => m.MyProfilePageModule)
           }
         ]
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/home',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/home',
     pathMatch: 'full'
   }
 ];
