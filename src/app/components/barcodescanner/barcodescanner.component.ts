0062274c5bc60ee70285643c5308ffa602158f4a
Ibraheem Ghaffar <S00179906@mail.itsligo.ie>
2019-10-22 23:03:42 +0100

    barcodescanner.html: imported barcodescanner, injected scanner into ctror, added function init func to ngOnInit

--- src/app/components/barcodescanner/barcodescanner.component.ts
+++ src/app/components/barcodescanner/barcodescanner.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-barcodescanner',
   templateUrl: './barcodescanner.component.html',
-  styleUrls: ['./barcodescanner.component.scss'],
+  styleUrls: ['./barcodescanner.component.scss']
 })
 export class BarcodescannerComponent implements OnInit {
+  constructor(private barcodeScanner: BarcodeScanner) {}
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  ngOnInit() {
+    this.barcodeScanner
+      .scan()
+      .then(barcodeData => {
+        console.log('Barcode data', barcodeData);
+      })
+      .catch(err => {
+        console.log('Error', err);
+      });
+  }
 }
