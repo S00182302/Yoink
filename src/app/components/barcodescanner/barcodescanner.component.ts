7044f2fe838647443e679789265f8c1b0551eecb
Ibraheem Ghaffar <S00179906@mail.itsligo.ie>
2019-10-22 23:01:58 +0100

    barcode scanner component created

---
+++ src/app/components/barcodescanner/barcodescanner.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-barcodescanner',
+  templateUrl: './barcodescanner.component.html',
+  styleUrls: ['./barcodescanner.component.scss'],
+})
+export class BarcodescannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
