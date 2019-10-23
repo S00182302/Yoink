ede82a91923dacf753beb3e58c53ce4e077e286b
Peter Snee <Peter Snee@DESKTOP-3OF2C5U>
2019-10-23 09:59:00 +0100

    search bar opens

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -6,10 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss']
 })
 export class HomePage {
-  hideMe = false;
-
+  FilterSection = false;
+  searchbar = false;
+  counties: string[] = [
+    'Antrim,',
+    'Armagh',
+    'Carlow',
+    'Cavan',
+    'Clare',
+    'Cork',
+    'Derry',
+    'Donegal',
+    'Down',
+    'Dublin',
+    'Fermanagh',
+    'Galway',
+    'Kerry',
+    'Kildare',
+    'Kilkenny',
+    'Laois',
+    'Leitrim',
+    'Limerick',
+    'Longford',
+    'Louth',
+    'Mayo',
+    'Meath',
+    'Monaghan',
+    'Offaly',
+    'Roscommon',
+    'Sligo',
+    'Tipperary',
+    'Tyrone',
+    'Waterford',
+    'Westmeath',
+    'Wexford',
+    'Wicklow'
+  ];
   constructor() {}
-  hide() {
-    this.hideMe = true;
+  showSearch() {
+    this.searchbar = true;
+  }
+  hideSearch() {
+    this.searchbar = false;
+  }
+  // to apply the filter options
+  applyFilters() {}
+
+  showFilterSection() {
+    this.FilterSection = true;
+  }
+  hideFilterSection() {
+    this.FilterSection = false;
   }
 }
