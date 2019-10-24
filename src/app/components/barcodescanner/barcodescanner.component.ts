9b5dedf8c222df2f4ccf7ee1a3775d3ca139b819
Ibraheem Ghaffar <S00179906@mail.itsligo.ie>
2019-10-24 22:26:10 +0100

    barcodescanner.ts: injected openfoodservice, added getFoodData method.

--- src/app/components/barcodescanner/barcodescanner.component.ts
+++ src/app/components/barcodescanner/barcodescanner.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { OpenfoodfactsService } from 'src/app/services/openfoodfacts.service';
 
 @Component({
   selector: 'app-barcodescanner',
@@ -7,17 +8,29 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
   styleUrls: ['./barcodescanner.component.scss']
 })
 export class BarcodescannerComponent implements OnInit {
-  constructor(private barcodeScanner: BarcodeScanner) {}
+  constructor(
+    private barcodeScanner: BarcodeScanner,
+    private openFoodDataService: OpenfoodfactsService
+  ) {}
+  barcode: string = '';
 
   openBarcodeScanner = () => {
     this.barcodeScanner
       .scan()
       .then(barcodeData => {
         console.log('Barcode data', barcodeData);
+        this.barcode = barcodeData.text;
+        this.getFoodData();
       })
       .catch(err => {
         console.log('Error', err);
       });
+  };
+
+  getFoodData = () => {
+    this.openFoodDataService
+      .getData(this.barcode)
+      .subscribe(data => console.log('The food data is:', data));
   };
 
   ngOnInit() {}
