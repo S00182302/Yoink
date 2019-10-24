475f43e28fec379ce6b4565eb13fec17855e9284
Ibraheem Ghaffar <S00179906@mail.itsligo.ie>
2019-10-24 15:21:11 +0100

    global.scss: more elements added for dark theme style.
    editprofile.html: added click event to checkmark button.
    
    editprofile.ts: added toast notifications.
    
    login.ts: added login progess spinner.

--- src/app/pages/editprofile/editprofile.page.ts
+++ src/app/pages/editprofile/editprofile.page.ts
@@ -3,6 +3,7 @@ import { ModalController, NavParams } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
 import { YoinkService } from 'src/app/services/yoink.service';
 import { StoredataService } from 'src/app/services/storedata.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-editprofile',
@@ -21,7 +22,8 @@ export class EditprofilePage implements OnInit {
     private route: ActivatedRoute,
     private navParams: NavParams,
     private yoinkService: YoinkService,
-    private storageService: StoredataService
+    private storageService: StoredataService,
+    public toastController: ToastController
   ) {
     console.log(navParams.get('id'));
   }
@@ -39,7 +41,7 @@ export class EditprofilePage implements OnInit {
         console.log(this.user);
       });
   };
-  
+
   genderChanged = (ev: any) => {
     console.log('Gender changed:', ev.detail.value);
   };
@@ -51,6 +53,45 @@ export class EditprofilePage implements OnInit {
       dismissed: true
     });
   };
+
+  editProfile = () => {
+    this.presentToast();
+  };
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Profile Updated',
+      duration: 2000,
+      color: 'dark'
+    });
+    toast.present();
+  }
+
+  // async presentToastWithOptions() {
+  //   const toast = await this.toastController.create({
+  //     header: 'Toast header',
+  //     message: 'Click to Close',
+  //     position: 'top',
+  //     buttons: [
+  //       {
+  //         side: 'start',
+  //         icon: 'star',
+  //         text: 'Favorite',
+  //         handler: () => {
+  //           console.log('Favorite clicked');
+  //         }
+  //       },
+  //       {
+  //         text: 'Done',
+  //         role: 'cancel',
+  //         handler: () => {
+  //           console.log('Cancel clicked');
+  //         }
+  //       }
+  //     ]
+  //   });
+  //   toast.present();
+  // }
 
   ngOnInit() {
     this.getAuth();
