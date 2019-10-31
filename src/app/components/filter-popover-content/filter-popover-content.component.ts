1d8f4b06904bf8dba082481d9f6f731a6bc3b8f7
Ibraheem Ghaffar <S00179906@mail.itsligo.ie>
2019-10-31 19:01:28 +0000

    created content component for filter popover

---
+++ src/app/components/filter-popover-content/filter-popover-content.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-popover-content',
+  templateUrl: './filter-popover-content.component.html',
+  styleUrls: ['./filter-popover-content.component.scss'],
+})
+export class FilterPopoverContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/controllers/popover/popover.component.ts
+++ src/app/controllers/popover/popover.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
 
 @Component({
   selector: 'app-popover',
   templateUrl: './popover.component.html',
-  styleUrls: ['./popover.component.scss'],
+  styleUrls: ['./popover.component.scss']
 })
 export class PopoverComponent implements OnInit {
+  constructor(public popoverController: PopoverController) {}
 
-  constructor() { }
+  async presentPopover(ev: any) {
+    const popover = await this.popoverController.create({
+      component: "PostPopoverContentComponent",
+      event: ev,
+      translucent: true
+    });
+    return await popover.present();
+  }
 
   ngOnInit() {}
-
 }
