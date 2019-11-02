908fba647ff27334944dbe5d43390ee77248af06
Ibraheem Ghaffar <S00179906@mail.itsligo.ie>
2019-11-02 00:11:33 +0000

    comments.ts: now getting the parameter passed from routerlink.
    comments.html: added comments template.

--- src/app/pages/comments/comments.page.ts
+++ src/app/pages/comments/comments.page.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.page.html',
-  styleUrls: ['./comments.page.scss'],
+  styleUrls: ['./comments.page.scss']
 })
 export class CommentsPage implements OnInit {
-
-  constructor() { }
+  postId: string;
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
+    this.postId = this.activatedRoute.snapshot.paramMap.get('id');
+    console.log(this.postId);
+    this.router.getCurrentNavigation
   }
-
 }
